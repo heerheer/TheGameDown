db0f1f95a09a1ec5a0b7f08320381b40784f98dd
runninghyk <running_hyk@hotmail.com>
2016-10-04 17:47:17 +0800

    Update main.java

--- src/com/running/download/main.java
+++ src/com/running/download/main.java
@@ -1,4 +1,5 @@
 package com.running.download;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -8,6 +9,15 @@ public class main {
 	private String dl0,dl1,dl2,dl3,dl4,dl5,dl6,dl7,dl8,dl9;
 	public static void main(String[] args) throws IOException{
 		String path = "./bin/dofile.ini";
+		File file=new File(path);    
+		if(!file.exists())    
+		{    
+		    try {    
+		        file.createNewFile();    
+		    } catch (IOException e) {    
+		        e.printStackTrace();    
+		    }    
+		}
 		Properties pp = new Properties();
 	    FileInputStream fis = new FileInputStream(path);
 		pp.load(fis);
